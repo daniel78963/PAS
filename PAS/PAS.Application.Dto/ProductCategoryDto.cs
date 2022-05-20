24c788a6fcf196d7c27c95fb38fc0913091eac2d
Daniel Alejandro Ramirez Ramirez <daniel78963@hotmail.com>
2022-05-19 20:37:32 -0500

    Dtos

---
+++ PAS/PAS.Application.Dto/ProductCategoryDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PAS.Application.Dto
+{
+    public class ProductCategoryDto : IEntityDto
+    {
+        public int Id { get; set; }
+        public string NameCategory { get; set; }
+
+    }
+}
---
+++ PAS/PAS.Application.Dto/ProductDto.cs
@@ -0,0 +1,14 @@
+﻿namespace PAS.Application.Dto
+{
+    public class ProductDto : IEntityDto        
+    {
+        public int Id { get ;  set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public string? ImageUrl { get; set; }
+
+        public int CategoryId { get; set; }
+        public ProductCategoryDto Categories { get; set; }
+
+    }
+}
