2099b479217e8c3a8fbfbcb6c859bcac906b1aff
Daniel Alejandro Ramirez Ramirez <daniel78963@hotmail.com>
2022-05-20 16:34:13 -0500

    IDataContext

---
+++ PAS/PAS.Infrastructure.Interfaces/IDataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PAS.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PAS.Infrastructure.Interfaces
+{
+    public interface IDataContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+
+        Task<int> SaveChanges();
+    }
+}
---
+++ PAS/PAS.Infrastructure.Interfaces/IProductCategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using PAS.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PAS.Infrastructure.Interfaces
+{
+    public interface IProductCategoryRepository
+    {
+        IEnumerable<ProductCategory> GetProductsCategories();
+        IEnumerable<ProductCategory> GetProductsCategories(string parameters);
+    }
+}
