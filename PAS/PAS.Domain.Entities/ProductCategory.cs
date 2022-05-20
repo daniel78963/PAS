dd1af342772fe5238cdfd2373aeb7186cb7a5c55
Daniel Alejandro Ramirez Ramirez <daniel78963@hotmail.com>
2022-05-20 12:01:14 -0500

    infrastructure.Data

---
+++ PAS/PAS.Domain.Entities/ProductCategory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PAS.Domain.Entities
+{
+    public class ProductCategory: BaseEntity
+    {
+        public string NameCategory { get; set; }
+    }
+}
---
+++ PAS/PAS.Infrastructure.Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PAS.Domain.Entities;
+
+namespace PAS.Infrastructure.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Product > Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
