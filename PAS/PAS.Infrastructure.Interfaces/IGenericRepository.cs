6f908e6db4f0e17cf11985248e87f84c3597b6b8
Daniel Alejandro Ramirez Ramirez <daniel78963@hotmail.com>
2022-05-24 18:39:05 -0500

    add IGenericRepository 1

---
+++ PAS/PAS.Infrastructure.Interfaces/IGenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq.Expressions;
+
+namespace PAS.Infrastructure.Interfaces
+{
+    public interface IGenericRepository<TEntity> where TEntity : class
+    {
+        //TODO: UPDATE ASYNC
+        IEnumerable<TEntity> All();
+        Task<IEnumerable<TEntity>> AllAsync();
+        TEntity GetById(object id);
+        Task<TEntity> GetByIdAsync(object id);
+        bool Add(TEntity entity);
+        Task<bool> AddAsync(TEntity entity);
+        bool Delete(object id);
+        Task<bool> DeleteAsync(object id);
+        //Task<bool> Upsert(TEntity entity);
+        void Update(TEntity entity);
+        //Task<IEnumerable<TEntity>> Find(Expression<Func<TEntity, bool>> predicate);
+    }
+}
---
+++ PAS/PAS.Infrastructure.Repositories/GenericRepository.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PAS.Infrastructure.Data;
+using PAS.Infrastructure.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PAS.Infrastructure.Repositories
+{
+    //public class GenericRepository<TEntity> where TEntity : class
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class
+    {
+        internal DataContext context;
+        internal DbSet<TEntity> dbSet;
+
+        public GenericRepository(DataContext context)
+        {
+            this.context = context;
+            this.dbSet = context.Set<TEntity>();
+        }
+
+        //public virtual IEnumerable<TEntity> Get(
+        //    Expression<Func<TEntity, bool>> filter = null,
+        //    Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+        //    string includeProperties = "")
+        //{
+        //    IQueryable<TEntity> query = dbSet;
+
+        //    if (filter != null)
+        //    {
+        //        query = query.Where(filter);
+        //    }
+
+        //    foreach (var includeProperty in includeProperties.Split
+        //        (new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+        //    {
+        //        query = query.Include(includeProperty);
+        //    }
+
+        //    if (orderBy != null)
+        //    {
+        //        return orderBy(query).ToList();
+        //    }
+        //    else
+        //    {
+        //        return query.ToList();
+        //    }
+        //}
+
+        public virtual IEnumerable<TEntity> All()
+        {
+            IQueryable<TEntity> query = dbSet;
+            return query.ToList();
+        }
+
+        public virtual async Task<IEnumerable<TEntity>> AllAsync()
+        {
+            IQueryable<TEntity> query = dbSet;
+            return await query.ToListAsync();
+        }
+
+        public virtual TEntity GetById(object id)
+        {
+            return dbSet.Find(id);
+        }
+
+        public virtual async Task<TEntity> GetByIdAsync(object id)
+        {
+            return await dbSet.FindAsync(id);
+        }
+
+        public virtual bool Add(TEntity entity)
+        {
+            dbSet.Add(entity);
+            return true;
+        }
+
+        public virtual async Task<bool> AddAsync(TEntity entity)
+        {
+            await dbSet.AddAsync(entity);
+            return true;
+        }
+
+        public virtual bool Delete(object id)
+        {
+            TEntity entityToDelete = dbSet.Find(id);
+            //Delete(entityToDelete);
+            if (entityToDelete == null) return false;
+
+            if (context.Entry(entityToDelete).State == EntityState.Detached)
+            {
+                dbSet.Attach(entityToDelete);
+            }
+            dbSet.Remove(entityToDelete);
+            return true;
+        }
+
+        public virtual async Task<bool> DeleteAsync(object id)
+        {
+            TEntity entityToDelete = await dbSet.FindAsync(id);
+            //Delete(entityToDelete);
+            if (entityToDelete == null) return false;
+
+            if (context.Entry(entityToDelete).State == EntityState.Detached)
+            {
+                dbSet.Attach(entityToDelete);
+            }
+            dbSet.Remove(entityToDelete);
+            return true;
+        }
+
+        //public virtual void Delete(TEntity entityToDelete)
+        //{
+        //    if (context.Entry(entityToDelete).State == EntityState.Detached)
+        //    {
+        //        dbSet.Attach(entityToDelete);
+        //    }
+        //    dbSet.Remove(entityToDelete);
+        //}
+
+        public virtual void Update(TEntity entityToUpdate)
+        {
+            dbSet.Attach(entityToUpdate);
+            context.Entry(entityToUpdate).State = EntityState.Modified;
+        }
+
+        //public virtual Task<bool> UpdateASync(TEntity entityToUpdate)
+        //{
+        //    //dbSet.Attach(entityToUpdate);
+        //    //context.Entry(entityToUpdate).State = EntityState.Modified;
+        //     dbSet.Update(entityToUpdate);
+        //    return   true;
+        //}
+    }
+}
