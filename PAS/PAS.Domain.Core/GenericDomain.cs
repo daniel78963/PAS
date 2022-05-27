448bb38884e432f639e6e1c80504d8205ed87e17
Daniel Alejandro Ramirez Ramirez <daniel78963@hotmail.com>
2022-05-27 18:43:53 -0500

    add IGenericDomain

---
+++ PAS/PAS.Domain.Core/GenericDomain.cs
@@ -0,0 +1,17 @@
+﻿using PAS.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PAS.Domain.Core
+{
+    public class GenericDomain<T> : IGenericDomain<T> where T : class
+    {
+        public Task<T> GetByIdAsync(object id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PAS/PAS.Domain.Interfaces/IGenericDomain.cs
@@ -0,0 +1,7 @@
+﻿namespace PAS.Domain.Interfaces
+{
+    public interface IGenericDomain<T> where T : class
+    {
+        Task<T> GetByIdAsync(object id);
+    }
+}
