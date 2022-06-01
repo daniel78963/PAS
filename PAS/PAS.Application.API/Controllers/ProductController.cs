6b3e9fc266fbe71c399b211a3ff740f6cf9baa24
Daniel Alejandro Ramirez Ramirez <daniel78963@hotmail.com>
2022-05-31 21:54:42 -0500

    add ProductController

---
+++ PAS/PAS.Application.API/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PAS.Application.Interfaces;
+
+namespace PAS.Application.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService productService;
+
+        public ProductController(IProductService productService)
+        {
+            this.productService = productService;
+        }
+
+        [HttpGet]   
+        public IActionResult GetByName(string name)
+        { 
+            return Ok(productService.GetByName(name));
+        }
+    }
+}
