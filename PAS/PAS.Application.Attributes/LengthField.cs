56454546c4a29850e5c29386f425ea9321b0e457
Daniel Alejandro Ramirez Ramirez <daniel78963@hotmail.com>
2022-06-06 16:28:27 -0500

    add RequiredField

---
+++ PAS/PAS.Application.Attributes/LengthField.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PAS.Application.Attributes
+{
+    public class LengthField :  CustomAttribute
+    {
+        public int MinValue { get; set; }
+        public int MaxValue { get; set; }
+    }
+}
--- PAS/PAS.Application.Dto/ProductCategoryDto.cs
+++ PAS/PAS.Application.Dto/ProductCategoryDto.cs
@@ -1,7 +1,10 @@
-﻿namespace PAS.Application.Dto
+﻿using PAS.Application.Attributes;
+
+namespace PAS.Application.Dto
 {
     public class ProductCategoryDto : BaseDto
     {
+        [RequiredField(Validate = true, Message = "Field NameCategory is required", Code = "1402")]
         public string NameCategory { get; set; }
     }
 }
