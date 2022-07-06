708b38a22a4fa955c7772d02285e9a1ee6682999
Daniel Alejandro Ramirez Ramirez <daniel78963@hotmail.com>
2022-07-06 09:49:00 -0500

    add IdentityDbContext

--- PAS/PAS.Infrastructure.Data/DataContext.cs
+++ PAS/PAS.Infrastructure.Data/DataContext.cs
@@ -1,12 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using PAS.Domain.Entities;
 using PAS.Infrastructure.Interfaces;
 
 namespace PAS.Infrastructure.Data
 {
     //public class DataContext : DbContext, IDataContext
-    public class DataContext : DbContext
-    //public class DataContext : IdentityDbContext
+    //public class DataContext : DbContext
+    public class DataContext : IdentityDbContext
     {
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductCategory> ProductCategory { get; set; }
