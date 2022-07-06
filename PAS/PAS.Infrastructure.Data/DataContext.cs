76747c6a1907db5308f188c1209218b3acb1a217
Daniel Alejandro Ramirez Ramirez <daniel78963@hotmail.com>
2022-07-06 12:43:53 -0500

    to do Migrations in DDD

--- PAS/PAS.Infrastructure.Data/DataContext.cs
+++ PAS/PAS.Infrastructure.Data/DataContext.cs
@@ -6,11 +6,12 @@ using PAS.Infrastructure.Interfaces;
 namespace PAS.Infrastructure.Data
 {
     //public class DataContext : DbContext, IDataContext
-    //public class DataContext : DbContext
-    public class DataContext : IdentityDbContext
+    public class DataContext : DbContext
+    //public class DataContext : IdentityDbContext
     {
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductCategory> ProductCategory { get; set; }
+        public DbSet<Book> Books { get; set; }
 
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
