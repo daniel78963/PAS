9d8c95c8b57fd4682a3caec7b673e2dec601067c
Daniel Alejandro Ramirez Ramirez <daniel78963@hotmail.com>
2022-07-06 10:37:22 -0500

    add Book entity

---
+++ PAS/PAS.Domain.Entities/Book.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PAS.Domain.Entities
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        [StringLength(maximumLength: 250)]
+        public string Title { get; set; }
+
+    }
+}
