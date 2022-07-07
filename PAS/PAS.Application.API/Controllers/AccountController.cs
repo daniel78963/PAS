a842a4d30e576ef7dd32c156994eb8f603c2117b
Daniel Alejandro Ramirez Ramirez <daniel78963@hotmail.com>
2022-07-07 10:54:18 -0500

    add UserCredentials

---
+++ PAS/PAS.Application.API/Controllers/AccountController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PAS.Application.Dto.Account;
+
+namespace PAS.Application.API.Controllers
+{
+    [ApiController]
+    [Route("api/account")]
+    public class AccountController : ControllerBase
+    {
+        public async Task<ActionResult<AuthenticationResponse>> Signin()
+        {
+            return View();
+        }
+    }
+}
---
+++ PAS/PAS.Application.Dto/Account/AuthenticationResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace PAS.Application.Dto.Account
+{
+    public class AuthenticationResponse
+    {
+        public string Token { get; set; }
+        public DateTime Expiration { get; set; }
+    }
+}
